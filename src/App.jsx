c0742a1fc2f121478041bf777d0295bad557c4ca
lkdtt <Julius.luo2019@outlook.com>
2025-05-21 16:48:36 -0400

    second update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import './App.css'
 
 function App() {
   return (
-    <div id = "backboard">
+    <div id="backboard">
+      <h1>ToDo List</h1>
+      <div className="input-group">
+        <input
+          type="text"
+          placeholder="Add your task"
+          /* …bind to state here… */
+        />
+        <button className="add-btn">+</button>
+      </div>
     </div>
   );
 }
